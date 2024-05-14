8b9036ee6c76129351fbd4652e9e4cb7b10f5069
Om Unadkat <omunadkat2@gmail.com>
2024-05-14 12:32:12 +0530

    module_!

---
+++ module_1/script.js
@@ -0,0 +1,23 @@
+
+        alert("Hello world ");
+
+        console.log("code is running...")
+        console.log("code is also  running...")
+        console.log("code is not running...")
+
+        var a = prompt("enter your number")
+
+        var isTrue = confirm("are u sure to leave your pc will blast")
+
+        if(isTrue)
+            {
+                console.log("ur pc will blast")
+            }
+
+            else
+            {
+                console.log("ur pc will not blast ")
+            }
+
+        console.log("your number is" + a)
+    
