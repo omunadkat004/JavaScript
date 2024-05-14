3c17cea3532fcdbe1f777255f38d5c153231391a
Om Unadkat <omunadkat2@gmail.com>
2024-05-14 17:50:42 +0530

    module 3

---
+++ module_3/script.js
@@ -0,0 +1,16 @@
+console.log("hello i am conditional tutotial ")
+
+let age =20
+let grace =2 
+console.log(age ** grace) 
+// 20^2 x1
+// "3"== 3 (True ) this nly compares value NOT TYPE 
+// "3" === 3 (False ) triple equal compares value and type 
+if((age-grace)>=18)
+    {
+        console.log("u can ..")
+    }
+else
+{
+    console.log("u cant..")
+}
