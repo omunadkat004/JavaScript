0e98ccbdb8dee7bb1fcd241980cf4a33b7217b07
Om Unadkat <omunadkat2@gmail.com>
2024-05-14 20:43:46 +0530

    module_5

---
+++ module_5/script.js
@@ -0,0 +1,36 @@
+function nice(name) {
+    console.log("hey " + name + " you are nice")
+    console.log("hey " + name + " you are very nice")
+    console.log("hey " + name + " you are nice too ")
+    
+}
+
+nice("anmol")
+
+function sum (a,b)
+{
+//    console.log(a+b) 
+return (a+b)
+}
+
+result = sum(4,5) 
+
+console.log("the sum of no is ", result) 
+
+let x=prompt("enter first no ")
+let y=prompt("enter 2nd no")
+
+let u =(x*y)
+let w = (x+y)
+let z = parseInt(x) + parseInt(y)
+console.log(u)
+console.log(w)
+console.log(z)
+
+const func1= (x) =>{
+    console.log("i am an arrow function ", x)
+    
+}
+func1(34);
+func1(44);
+func1(54);
