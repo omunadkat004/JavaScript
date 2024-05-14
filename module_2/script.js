e70f7f2d15956826c7054071aab10cb111f3e345
Om Unadkat <omunadkat2@gmail.com>
2024-05-14 16:55:15 +0530

    adding module_2

---
+++ module_2/script.js
@@ -0,0 +1,41 @@
+console.log("this is my JS learning")
+
+var a =6
+var b =5
+var c ="Om"
+
+console.log(a+b+1)
+{
+    // let variable khali aa bracket ni andar j a ni value 6 chalavse 
+    let a =66
+    console.log(a)
+}
+
+console.log (typeof a , typeof b , typeof c)
+
+
+let x = "Om";
+let y = 22;
+let z = 3.55 ;
+const p = true ;
+let q = undefined ;
+let r = null ;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r,)  
+
+// SEE NULL HAS TYPE OF OBJECT IN JAVASCRIPT , WHY ? 
+
+
+//  OBJECT 
+
+let o = {
+    "name": "Om ",
+    // here name is key and om is its value 
+    "job code" : 5600,
+    "its_handsome":true,
+}
+
+console.log(o)
+o.salary = "100cr"
+console.log(o)
