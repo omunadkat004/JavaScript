d94229aa597d70f0ed9f7266a44874c5f5bb3e9e
Om Unadkat <omunadkat2@gmail.com>
2024-05-15 15:35:02 +0530

    adding module_6

---
+++ module_6/script.js
@@ -0,0 +1,26 @@
+console.log("this is about strings")
+let a ="Myname is Om"
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+console.log(a.length)
+
+let real_mame = "OM"
+let frd = "anmol" 
+console.log( real_mame + "and" ,frd + "are friends" )
+// console.log(`his name is`, $(real_mame)`and his frd is ` ,$(frd))
+
+let c ="shivam"
+
+console.log(c.toUpperCase())
+console.log(c.toLowerCase())
+
+console.log(c.slice(1,4))
+console.log(c.replace("shi", "777"))
+console.log(c.concat(a))
+console.log(c.concat(a, "khushi"))
+// strings are immutable ek var bani gai to emni orginal value never changes 
