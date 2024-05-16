06a651a9be90bde7cbe55271f7e84c59d9053d3d
Om Unadkat <omunadkat2@gmail.com>
2024-05-16 11:17:27 +0530

    module_10

--- module_10/script.js
+++ module_10/script.js
@@ -3,13 +3,13 @@
 // let bhupendrajogis = document.getElementsByClassName("box")
 // console.log(bhupendrajogis)
 
-bhupendrajogis[2].style.backgroundColor="red"
+// bhupendrajogis[2].style.backgroundColor="red"
 // will color red whichever is 2nd index 
 
-document.getElementById("blue").style.backgroundColor="blue"
+// document.getElementById("blue").style.backgroundColor="blue"
 // will color blue whichever has id blue 
 
-document.querySelector(".box").style.backgroundColor="orange"
+// document.querySelector(".box").style.backgroundColor="orange"
 // colors orange where first class is matched as box 
 
 //  if we want to color all boxes orange 
@@ -18,10 +18,10 @@ document.querySelector(".box").style.backgroundColor="orange"
 //  .style of array , string , html collection dosent mean anything .style only works on elements 
 
 // Thus to color all of them pink :
-document.querySelectorAll(".box").forEach(e => {
-    e.style.backgroundColor="pink"
-});
+// document.querySelectorAll(".box").forEach(e => {
+    // e.style.backgroundColor="pink"
+// });
 
-document.getElementsByTagName("div")
+// document.getElementsByTagName("div")
 
 // Matches , contains , closest 
