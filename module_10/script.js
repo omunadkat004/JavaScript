7f56c5a6f44fed0833794ad405d1eb6be16669f9
Om Unadkat <omunadkat2@gmail.com>
2024-05-16 11:13:38 +0530

    module_10

--- module_10/script.js
+++ module_10/script.js
@@ -1,18 +1,29 @@
-console.log("Omm")
-
-let boxes = document.getElementsByClassName("box")
-console.log(boxes)
-
-boxes[2].style.backgroundColor = "red"
-
-document.getElementById("redbox").style.backgroundColor = "red"
+console.log("omm")
 
 
-document.querySelector(".box").style.backgroundColor = "green";
+let bhupendrajogis = document.getElementsByClassName("box")
+console.log(bhupendrajogis)
 
-console.log(document.querySelectorAll(".box"))
 
+bhupendrajogis[2].style.backgroundColor="red"
+// will color red whichever is 2nd index 
 
-document.querySelectorAll(".box").forEach(e =>{
-    e.style.backgroundColor = "green";
-}) 
+document.getElementById("blue").style.backgroundColor="blue"
+// will color blue whichever has id blue 
+
+document.querySelector(".box").style.backgroundColor="orange"
+// colors orange where first class is matched as box 
+
+//  if we want to color all boxes orange 
+// document.querySelectorAll(".box").style.backgroundColor="pink"
+// the above wont work bcoz it selects html collection and this works on elements not on html collection 
+//  .style of array , string , html collection dosent mean anything .style only works on elements 
+
+// Thus to color all of them pink :
+document.querySelectorAll(".box").forEach(e => {
+    e.style.backgroundColor="pink"
+});
+
+document.getElementsByTagName("div")
+
+// Matches , contains , closest 
